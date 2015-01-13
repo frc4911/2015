abcf08699368480c009ab40dabd55b501b0bf060
AaronBae <abae777@gmail.com>
2015-01-12 16:39:41 -0800

    Aaron uploading first Testing Code for the Team

---
+++ Practice Codes - Aaron/FirstRoboRioProject/src/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	
+	public static final int LEFT_ENCODER_PORT_A = 14;//Digital //1
+	public static final int LEFT_ENCODER_PORT_B = 13;//Digital //2
+	public static final int RIGHT_ENCODER_PORT_A = 1;//Digital //3
+	public static final int RIGHT_ENCODER_PORT_B = 2;//Digital //4
+}
+
