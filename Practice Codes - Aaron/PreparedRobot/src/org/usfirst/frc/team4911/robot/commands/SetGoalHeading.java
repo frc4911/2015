a929e285e7e5dcd22191c07f7874a2143ced3de8
Jack Holt <jackholt647@gmail.com>
2015-01-31 10:47:16 -0800

    pushing dat code

--- Practice Codes - Aaron/PreparedRobot/src/org/usfirst/frc/team4911/robot/commands/SetGoalHeading.java
+++ Practice Codes - Aaron/PreparedRobot/src/org/usfirst/frc/team4911/robot/commands/SetGoalHeading.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4911.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team4911.robot.Robot;
+import org.usfirst.frc.team4911.robot.subsystems.MecanumDriveSystem;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
@@ -10,19 +11,31 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class SetGoalHeading extends Command {
 
 	private double goalHeading;
+	private double deltaGoalHeading;
 	private JoystickButton button;
+	private MecanumDriveSystem mecanumDriveSystem;
 	
-    public SetGoalHeading(double goalHeading, JoystickButton button) {
-		this.goalHeading = goalHeading;
+    public SetGoalHeading(double deltaGoalHeading, JoystickButton button) {
+		this.deltaGoalHeading = deltaGoalHeading;
+		this.button = button;
+    }
+    public SetGoalHeading(int isPositive, JoystickButton button) {
+		this.goalHeading = isPositive * 90;
 		this.button = button;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	mecanumDriveSystem = Robot.mecanumDriveSystem;
+		this.goalHeading = (deltaGoalHeading + mecanumDriveSystem.getGoalHeading()) % 180 ;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.printSystem.print("DeltaGoalHeading", "" + goalHeading);
+
+    	Robot.printSystem.print("GoalHeading", "" + mecanumDriveSystem.getGoalHeading());
+    	
     	Robot.mecanumDriveSystem.setGoalHeading(goalHeading);
     	
     }
