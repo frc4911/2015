ccf6dc69f7c760379f30a4a74cac041754571dd2
Jack Holt <jackholt647@gmail.com>
2015-02-15 18:16:10 -0800

    pulling camera code

--- Practice Codes - Aaron/PreparedRobot/src/org/usfirst/frc/team4911/robot/commands/AutoDriveIn.java
+++ Practice Codes - Aaron/PreparedRobot/src/org/usfirst/frc/team4911/robot/commands/AutoDriveIn.java
@@ -19,23 +19,15 @@ public class AutoDriveIn extends CommandGroup {
 	//	  |		|____	-----	  |
 	//
 	////////////////////////////////////////////////////////////////////////
+	
     public  AutoDriveIn(double stagingZones) {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	
+    	// ======================= STARTING POSITION INSTRUCTIONS ===========================
+    	// For this command the robot will slide down the staging zone, into the autonomous
+    	// zone, and then back down the autonomous zone. For this the robot must be placed
+    	// a certain distance down the staging zone from the left proportional to the parameter
+    	// stagingZones 
+    	
     	addSequential(new PIDAxisDrive(-0.5, 0.0, RobotConstants.DRIVE_ONE_STAGE_ZONE * stagingZones));
     	addSequential(new SetGoalHeading(90));
     	addSequential(new PIDAxisDrive(0.0, 0.5,  1024.0*30.5*3.0));
